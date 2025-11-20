eefdf5c1ab5d43ab06f0c5fcaff9a3f4949e4e1a
dibyanshu yadav <dibyanshuy85@gmail.com>
2025-11-20 20:53:47 +0530

    hello .db

--- blog/database.py
+++
@@ -1,8 +0,0 @@
-# from sqlmodel import create_engine, Session, SQLModel
-
-# DATABASE_URL = "sqlite:///./blog.db"
-# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-# def get_session():
-#     with Session(engine) as session:
-#         yield session
